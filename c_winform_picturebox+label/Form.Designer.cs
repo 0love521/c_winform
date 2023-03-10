81d29a8cbe4d68871e09083198052da1e6f4767e
olove521 <olove521@outlook.com>
2023-03-11 00:25:14 +0800

    c-winform-picturebox-label
    winform~picturebox~label

---
+++ c_winform_picturebox+label/Form.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace c_winform_picturebox_label
+{
+    partial class Form
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("华文彩云", 21.78151F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(214, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(301, 37);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "picturebox+label";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.pictureBox3.Image = global::c_winform_picturebox_label.Properties.Resources.NImg_transparent;
+            this.pictureBox3.Location = new System.Drawing.Point(452, 201);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.PictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox2.Image = global::c_winform_picturebox_label.Properties.Resources.NImg_transparent;
+            this.pictureBox2.Location = new System.Drawing.Point(335, 201);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.PictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox1.Image = global::c_winform_picturebox_label.Properties.Resources.Tick_block;
+            this.pictureBox1.Location = new System.Drawing.Point(221, 201);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "picturebox+label";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ c_winform_picturebox+label/Form.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using c_winform_picturebox_label.Properties;
+
+namespace c_winform_picturebox_label
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        public void Skins(int colorcode)
+        {
+            switch (colorcode)
+            {
+                case 1:
+                    label1.ForeColor = Color.FromArgb(169, 169, 169); //灰色
+                    break;
+
+                case 2:
+                    label1.ForeColor = Color.FromArgb(153, 180, 209); //浅蓝色
+
+                    break;
+
+                case 3:
+                    label1.ForeColor = Color.FromArgb(0, 120, 215); //蓝色
+                    break;
+            }
+        }
+
+        public void Setimg(PictureBox pictureBox)
+        {
+            Image Sselect = Resources.Tick_block;
+            Image uSelect = Resources.NImg_transparent;
+            pictureBox.Image = Sselect;
+            PictureBox[] pictureBoxes = { pictureBox1, pictureBox2, pictureBox3 };
+            foreach (PictureBox p in pictureBoxes)
+            {
+                if (p.Name != pictureBox.Name)
+                {
+                    p.Image = uSelect;
+                }
+            }
+        }
+
+        public void Setbtn(int code)
+        {
+            switch (code)
+            {
+                case 1:
+                    Setimg(pictureBox1);
+                    Skins(code);
+                    break;
+
+                case 2:
+                    Setimg(pictureBox2);
+                    Skins(code);
+                    break;
+
+                case 3:
+                    Setimg(pictureBox3);
+                    Skins(code);
+                    break;
+            }
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            Setbtn(1);
+        }
+
+        private void PictureBox2_Click(object sender, EventArgs e)
+        {
+            Setbtn(2);
+        }
+
+        private void PictureBox3_Click(object sender, EventArgs e)
+        {
+            Setbtn(3);
+        }
+    }
+}
